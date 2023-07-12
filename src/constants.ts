d177af6fb3c170bc55fd03425f67032cb249206e
Kai <zzev@proton.me>
2023-07-11 18:40:40 -0600

    Navigation listen to button clicks

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const LOCALES = ["es", "en"];
 
 export const DEFAULT_LOCALE = "es";
+
+export const DESKTOP_WIDTH = 1008;
---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+import { DESKTOP_WIDTH } from "../constants";
+
+const handleToggleMenu = (element) => {
+  if(window.innerWidth < DESKTOP_WIDTH) return;
+
+  element.classList.remove('nav-mobile-open');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const nav = document.getElementById('nav');
+  const burgerButton = document.getElementById('nav-burger');
+  const closeButton = document.getElementById('nav-close');
+
+  window.addEventListener('resize', () => { handleToggleMenu(nav) });
+
+  burgerButton.addEventListener('click', () => {
+    nav.classList.add('nav-mobile-open');
+  });
+
+  closeButton.addEventListener('click', () => {
+    nav.classList.remove('nav-mobile-open');
+  })
+});
