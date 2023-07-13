dd39a671f285a999e64292e343589b10fa4b6104
Gabriela Borraz <139186642+gabrielaborraz@users.noreply.github.com>
2023-07-12 21:29:14 -0600

    Translations uptaded and footer links added

--- src/utils/i18n/ui.ts
+++ src/utils/i18n/ui.ts
@@ -1,27 +1,52 @@
 export const ui = {
   es: {
+    /***********HOME***********/
     "home.title": "Inicio | Gabriela Borraz",
     "home.description":
       "Bienvenidos al sitio web de Gabriela Borraz, donde las ideas creativas cobran vida para comunicar, inspirar y conectar",
     "home.hero":
       "Las ideas cobran vida <span>para comunicar / inspirar / conectar</span>",
-    "about.title": "Sobre mí | Gabriela Borraz",
+    "home.featured.title": "Lo más destacado",
+    "home.featured.subtitle": "Proyectos de animación",
+    "home.show-reel.title": "Video",
+    "home.show-reel.subtitle": "Show reel 2023",
+    "home.collaborations.title": "Colaboraciones exitosas",
+    "home.collaborations.subtitle": "Con grandes marcas y clientes",
+    "home.newest.title":"Lo más reciente",
+    "home.newest.subtitle":"Galería de proyectos",
+    "home.contact.title":"Escríbeme",
+    "home.contact.subtitle":"Y creemos cosas increíbles",
+    /***********ABOUT***********/
+      "about.title": "Sobre mí | Gabriela Borraz",
     "about.description":
       "Animadora versátil de gráficos por computadora que da vida a ideas con creatividad y destreza. Descubre mi trabajo de animación, ilustración y diseño",
-    "contact.title": "En contacto",
+    /***********CONTACT***********/
+      "contact.title": "En contacto",
     "contact.description":
       "Escríbeme, me encantaría saber sobre tu próximo proyecto",
+    /***********FAQ***********/
     "faq.title": "Preguntas frecuentes",
     "faq.description":
       "Respondiendo a Preguntas Frecuentes sobre nuestros servicios de animación y diseño",
+    "faq.header.title":"Respondiendo",
+    "faq.header.subtitle":"A preguntas frecuentes",
+    /***********PORTFOLIO***********/
     "portfolio.index.title": "Portafolio de Proyectos",
     "portfolio.index.description":
       "Explora mi portafolio de proyectos y descubre cómo puedo ayudarte a producir una amplia variedad de videos y dar vida a tus ideas",
+    "portfolio.hero":"La magia de los videos animados <span>comienza con una idea creativa</span>",
+    "portfolio.multimedia.title":"Explora",
+    "portfolio.multimedia.subtitle":"Mi portafolio multimedia",
+    "portfolio.design.title":"Descubre",
+    "portfolio.design.subtitle":"Mi portafolio de diseño",
+    /***********DESIGN***********/
     "porfolio.design.index.title": "Portafolio de diseño",
     "porfolio.design.index.description": "Explora mis proyectos de diseño",
+    /***********MULTIMEDIA***********/
     "portfolio.multimedia.index.title": "Portafolio multimedia",
     "portfolio.multimedia.index.description":
       "Descubre la magia de los videos animados",
+    /***********PROJECTS***********/
     "portfolio.project.palabras-magicas.title":
       "Palabras Mágicas | Serie animada infantil",
     "portfolio.project.palabras-magicas.description":
@@ -35,40 +60,69 @@ export const ui = {
     "portfolio.project.viola-fermin-rdz.title": "El Viaje de la viola",
     "portfolio.project.viola-fermin-rdz.description":
       "La viola: de la música de la Nueva España al folklore chiapaneco",
+    /***********BLOG***********/
     "blog.index.title": "Blog: mi diario creativo",
     "blog.index.description":
       "Espacio abierto para compartir y aprender sobre animación, diseño, ilustración  y narrativa",
+    "blog.hero":"Experimenta. Explora. <span>Evoluciona.</span>",
+    "blog.header.title":"Playground",
+    "blog.header.subtitle":"Mi diario creativo",
     "blog.post.welcome.title": "Bienvenida al blog",
     "blog.post.welcome.description": "Hola mundo",
     "blog.post.animation-superpowers.title": "Los superpoderes de la animación",
     "blog.post.animation-superpowers.description": "Hola mundo",
   },
   en: {
+    /***********HOME***********/
     "home.title": "Home | Gabriela Borraz",
     "home.description":
       "Welcome to Gabriela Borraz's website, where creative ideas come to life to communicate, inspire and connect",
     "home.hero":
       "Creative ideas come to life <span>to communicate / inspire / connect</span>",
+    "home.featured.title": "The most outstanding",
+    "home.featured.subtitle": "Animation projects",
+    "home.show-reel.title": "Video",
+    "home.show-reel.subtitle": "Show reel 2023",
+    "home.collaborations.title": "Successful collaborations",
+    "home.collaborations.subtitle": "With great brands and clients",
+    "home.newest.title":"The latest",
+    "home.newest.subtitle":"Project gallery",
+    "home.contact.title":"Get in touch",
+    "home.contact.subtitle":"And let's create amazing things",
+    /***********ABOUT***********/
     "about.title": "About | Gabriela Borraz",
     "about.description":
       "Versatile digital animator who brings ideas to life with creativity and expertise. Discover my work in animation, illustration and design",
+    /***********CONTACT***********/
     "contact.title": "Get in touch",
     "contact.description":
       "Send me an email, I would love to hear about your upcoming project",
+    /***********FAQ***********/
     "faq.title": "Frequently asked questions",
     "faq.description":
       "Answering Frequently Asked Questions about our animation and design services",
+    "faq.header.title":"Answering",
+    "faq.header.subtitle":"Frequently Asked Questions",
+      /***********PORTFOLIO***********/
     "portfolio.index.title": "Project Portfolio ",
     "portfolio.index.description":
       "Explore my portfolio of projects and discover how I can help you to produce a wide variety of videos and bring your ideas to life",
+    "portfolio.hero":"The magic of animated videos<span>starts with a creative idea</span>",   
+    "portfolio.multimedia.title":"Explore",
+    "portfolio.multimedia.subtitle":"My multimedia portfolio",
+    "portfolio.design.title":"Discover",
+    "portfolio.design.subtitle":"My design portfolio",
+    /***********DESIGN***********/
     "porfolio.design.index.title": "Design Portfolio",
     "porfolio.design.index.description": "Explore my design projects",
+    /***********MULTIMEDIA***********/
     "portfolio.multimedia.index.title": "Multimedia Portfolio",
     "portfolio.multimedia.index.description":
       "Discover the magic of animated videos",
     "portfolio.project.palabras-magicas.title": "Magic Words | Kids animation",
     "portfolio.project.palabras-magicas.description":
       "Animated show for Tutorah Kids",
+    /***********PROJECTS***********/
     "portfolio.project.mpk.title": "Midot Power Kids",
     "portfolio.project.mpk.description": "Animated show for Tutorah Kids",
     "portfolio.project.tales-milly-cohen.title": "The Tales of Milly Cohen",
@@ -77,9 +131,13 @@ export const ui = {
     "portfolio.project.viola-fermin-rdz.title": "The journey of the viola",
     "portfolio.project.viola-fermin-rdz.description":
       "The viola: from the music of New Spain to Chiapas folklore nowadays",
+    /***********BLOG***********/
     "blog.index.title": "Blog: my creative journal",
     "blog.index.description":
       "An open space to share and learn about animation, design, illustration and storytelling",
+    "blog.hero":"Experiment. Explore. <span>Evolve.</span>",
+    "blog.header.title":"Playground",
+    "blog.header.subtitle":"My creative journal",
     "blog.post.bienvenida.title": "Welcome to blog",
     "blog.post.bienvenida.description": "Hello world",
     "blog.post.animation-superpowers.title": "The superpowers of animation",
