b880ba91c18a1caea3a968a2e75e043eaa139c22
Gabriela Borraz <139186642+gabrielaborraz@users.noreply.github.com>
2023-07-18 15:19:16 -0600

    Add translations to FORM

--- src/utils/i18n/ui.ts
+++ src/utils/i18n/ui.ts
@@ -31,6 +31,10 @@ export const ui = {
     "contact.title": "En contacto",
     "contact.description":
       "Escríbeme, me encantaría saber sobre tu próximo proyecto",
+    "contact.input.name":"Nombre*",
+    "contact.input.email":"Correo electrónico*",
+    "contact.input.message":"Escribe aquí tu mensaje*",
+    "contact.btn":"Enviar",
     /***********FAQ***********/
     "faq.title": "Preguntas frecuentes",
     "faq.description":
@@ -142,6 +146,10 @@ export const ui = {
     "contact.title": "Get in touch",
     "contact.description":
       "Send me an email, I would love to hear about your upcoming project",
+    "contact.input.name":"Name*",
+    "contact.input.email":"Email address*",
+    "contact.input.message":"Type your message here*",
+    "contact.btn":"Send",
     /***********FAQ***********/
     "faq.title": "Frequently asked questions",
     "faq.description":
