ff48c5c9154543a31ecdb45d64d0ff57622aca47
Kai <zzev@proton.me>
2023-07-21 21:40:27 -0600

    Add class to hero

--- src/utils/i18n/ui/home.ts
+++ src/utils/i18n/ui/home.ts
@@ -1,8 +1,10 @@
 export const home = {
   es: {
     "home.seo.title": "Inicio | Gabriela Borraz",
-    "home.seo.description": "Bienvenidos al sitio web de Gabriela Borraz, donde las ideas creativas cobran vida para comunicar, inspirar y conectar",
-    "home.hero": "Las ideas cobran vida <span>para comunicar / inspirar / conectar</span>",
+    "home.seo.description":
+      "Bienvenidos al sitio web de Gabriela Borraz, donde las ideas creativas cobran vida para comunicar, inspirar y conectar",
+    "home.hero":
+      "Las ideas cobran vida <span class='text-highlight-color'>para comunicar / inspirar / conectar</span>",
     "home.featured.title": "Lo más destacado",
     "home.featured.subtitle": "Proyectos de animación",
     "home.show-reel.title": "Video",
@@ -12,13 +14,16 @@ export const home = {
     "home.newest.title": "Lo más reciente",
     "home.newest.subtitle": "Galería de proyectos",
     "home.newest.btn": "Ir al Portafolio →",
-    "home.cta.text": "<br>Para crear contenido visual</br> que hará que tu marca destaque",
+    "home.cta.text":
+      "<br>Para crear contenido visual</br> que hará que tu marca destaque",
     "home.cta.btn": "Contacta conmigo →",
   },
   en: {
     "home.seo.title": "Home | Gabriela Borraz",
-    "home.seo.description": "Welcome to Gabriela Borraz's website, where creative ideas come to life to communicate, inspire and connect",
-    "home.hero": "Creative ideas come to life <span>to communicate / inspire / connect</span>",
+    "home.seo.description":
+      "Welcome to Gabriela Borraz's website, where creative ideas come to life to communicate, inspire and connect",
+    "home.hero":
+      "Creative ideas come to life <span class='text-highlight-color'>to communicate / inspire / connect</span>",
     "home.featured.title": "The most outstanding",
     "home.featured.subtitle": "Animation projects",
     "home.show-reel.title": "Video",
@@ -28,7 +33,8 @@ export const home = {
     "home.newest.title": "The latest",
     "home.newest.subtitle": "Project gallery",
     "home.newest.btn": "Go to Portfolio →",
-    "home.cta.text": "<br>To create visual content<br/> that will make your brand stand out",
+    "home.cta.text":
+      "<br>To create visual content<br/> that will make your brand stand out",
     "home.cta.btn": "Drop me a line →",
   },
 };
--- src/utils/i18n/ui/portfolio.ts
+++ src/utils/i18n/ui/portfolio.ts
@@ -1,8 +1,10 @@
 export const portfolio = {
   es: {
     "portfolio.seo.title": "Portafolio de Proyectos",
-    "portfolio.seo.description": "Explora mi portafolio de proyectos y descubre cómo puedo ayudarte a producir una amplia variedad de videos y dar vida a tus ideas",
-    "portfolio.hero": "La magia de los videos animados <span>comienza con una idea creativa</span>",
+    "portfolio.seo.description":
+      "Explora mi portafolio de proyectos y descubre cómo puedo ayudarte a producir una amplia variedad de videos y dar vida a tus ideas",
+    "portfolio.hero":
+      "La magia de los videos animados <span class='text-highlight-color'>comienza con una idea creativa</span>",
     "portfolio.multimedia.title": "Explora",
     "portfolio.multimedia.subtitle": "Mi portafolio multimedia",
     "portfolio.design.title": "Descubre",
@@ -12,15 +14,16 @@ export const portfolio = {
     "porfolio.design.seo.description": "Explora mis proyectos de diseño",
     /***********MULTIMEDIA***********/
     "portfolio.multimedia.seo.title": "Portafolio multimedia",
-    "portfolio.multimedia.seo.description": "Descubre la magia de los videos animados",
+    "portfolio.multimedia.seo.description":
+      "Descubre la magia de los videos animados",
     /***********BUTTONS AND BREADCRUMB***********/
     "portfolio.project.synopsis": "Sinopsis",
     "portfolio.project.description": "Descripción",
-    "breadcrumb.home":"Inicio",
-    "breadcrumb.portfolio":"Portafolio",
-    "breadcrumb.multimedia":"Multimedia",
-    "breadcrumb.design":"Diseño",
-    "portfolio.external-btn" : "Ver →",
+    "breadcrumb.home": "Inicio",
+    "breadcrumb.portfolio": "Portafolio",
+    "breadcrumb.multimedia": "Multimedia",
+    "breadcrumb.design": "Diseño",
+    "portfolio.external-btn": "Ver →",
     /***********CREDIT TABLE***********/
     "portfolio.table-cell.year": "Año",
     "portfolio.table-cell.client": "Cliente",
@@ -30,8 +33,10 @@ export const portfolio = {
   },
   en: {
     "portfolio.seo.title": "Project Portfolio ",
-    "portfolio.seo.description": "Explore my portfolio of projects and discover how I can help you to produce a wide variety of videos and bring your ideas to life",
-    "portfolio.hero": "The magic of animated videos<span>starts with a creative idea</span>",
+    "portfolio.seo.description":
+      "Explore my portfolio of projects and discover how I can help you to produce a wide variety of videos and bring your ideas to life",
+    "portfolio.hero":
+      "The magic of animated videos<span class='text-highlight-color'>starts with a creative idea</span>",
     "portfolio.multimedia.title": "Explore",
     "portfolio.multimedia.subtitle": "My multimedia portfolio",
     "portfolio.design.title": "Discover",
@@ -41,15 +46,16 @@ export const portfolio = {
     "porfolio.design.seo.description": "Explore my design projects",
     /***********MULTIMEDIA***********/
     "portfolio.multimedia.seo.title": "Multimedia Portfolio",
-    "portfolio.multimedia.seo.description": "Discover the magic of animated videos",
+    "portfolio.multimedia.seo.description":
+      "Discover the magic of animated videos",
     /***********BUTTONS AND BREADCRUMB***********/
     "portfolio.project.synopsis": "Synopsis",
     "portfolio.project.description": "Description",
-    "breadcrumb.home":"Home",
-    "breadcrumb.portfolio":"Portfolio",
-    "breadcrumb.multimedia":"Multimedia",
-    "breadcrumb.design":"Design",
-    "portfolio.external-btn" : "Watch →",
+    "breadcrumb.home": "Home",
+    "breadcrumb.portfolio": "Portfolio",
+    "breadcrumb.multimedia": "Multimedia",
+    "breadcrumb.design": "Design",
+    "portfolio.external-btn": "Watch →",
     /***********CREDIT TABLE***********/
     "portfolio.table-cell.year": "Year",
     "portfolio.table-cell.client": "Client",
