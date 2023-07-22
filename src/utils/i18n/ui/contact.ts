a9116f6e82da8c635afd754d0ebbb9bf5ada4464
Gabriela Borraz <139186642+gabrielaborraz@users.noreply.github.com>
2023-07-21 20:54:14 -0600

    Upgrade contact translations

--- src/utils/i18n/ui/contact.ts
+++ src/utils/i18n/ui/contact.ts
@@ -1,20 +1,28 @@
 export const contact = {
   es: {
-    "contact.title": "En contacto",
-    "contact.description":
-      "Escríbeme, me encantaría saber sobre tu próximo proyecto",
+    "contact.seo.title": "En contacto | Gabriela Borraz",
+    "contact.seo.description": "Escríbeme, me encantaría saber sobre tu próximo proyecto",
+    "contact.header.title": "Escríbeme",
+    "contact.header.subtitle":"Y creemos cosas increíbles",
     "contact.input.name": "Nombre*",
     "contact.input.email": "Correo electrónico*",
     "contact.input.message": "Escribe aquí tu mensaje*",
-    "contact.btn": "Enviar",
+    "contact.btn": "Enviar →",
+    "contact.data-sendig": "Enviando...",
+    "contact.alert-success":"<p>¡Gracias! Tu mensaje ha sido enviado con éxito.</p> <p>Nos pondremos en contacto contigo muy pronto.</p>",
+    "contact.alert-error":"<p>Ha habido un error al enviar el formulario :(</p> <p>Por favor, inténtalo de nuevo más tarde.</p>",
   },
   en: {
-    "contact.title": "Get in touch",
-    "contact.description":
-      "Send me an email, I would love to hear about your upcoming project",
+    "contact.seo.title": "Get in touch | Gabriela Borraz",
+    "contact.seo.description": "Send me an email, I would love to hear about your upcoming project",
+    "contact.header.title": "Get in touch",
+    "contact.header.subtitle":"And let's create amazing things",
     "contact.input.name": "Name*",
     "contact.input.email": "Email address*",
     "contact.input.message": "Type your message here*",
-    "contact.btn": "Send",
+    "contact.btn": "Send →",
+    "contact.data-sendig": "Sending email...",
+    "contact.alert-success":"<p>Thank you! Your message has been successfully sent.</p> <p> We will get in touch with you very soon.</p>",
+    "contact.alert-error":"<p>There has been an error while submitting the form :(</p> <p> Please try again later.</p>",
   },
 };
