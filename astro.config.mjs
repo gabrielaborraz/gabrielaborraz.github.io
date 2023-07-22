139151e142272e8f144e25dee491263e4a2852ba
Kai <zzev@proton.me>
2023-07-22 13:26:38 -0600

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+
+import { DEFAULT_LOCALE } from './src/constants';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [
+    sitemap({
+      lastmod: new Date(),
+      i18n: {
+        defaultLocale: DEFAULT_LOCALE,
+        locales: { es: 'es', en: 'en' }
+      },
+    })
+  ],
   site: 'https://gabrielaborraz.github.io'
 });
