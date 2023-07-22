e4b651aaacbbf6fec1a47208db72d770c8e96648
Gabriela Borraz <139186642+gabrielaborraz@users.noreply.github.com>
2023-07-21 20:57:12 -0600

    Upgrade PORTFOLIO: Multimedia/Design content pages

--- src/utils/content/projects.ts
+++ src/utils/content/projects.ts
@@ -21,8 +21,8 @@ const project = (args: ProjectArgsType): Project => {
     },
     name: t(`projects.${slug}.name`),
     summary: t(`projects.${slug}.summary`),
-    year: t(`projects.${slug}.year`),
-    client: t(`projects.${slug}.client`),
+    year: "",
+    client: "",
     category: t(`projects.${slug}.category`),
     synopsis: t(`projects.${slug}.synopsis`),
     imageSrc: `/projects/${slug}.jpg`,
@@ -45,16 +45,17 @@ export const projectMagicWords = (args?: { locale?: Locale }) => {
 
   return {
     ...info,
+    year: "2017",
+    client: "Tutorah Kids",
     videoUrl: "https://www.youtube.com/watch?v=PxmXOsvjJx8",
     illustration: ["Daniel Corkidi", "Rafael Castillo"],
     animation: ["Alejandro Olivares", "Gabriela Borraz"],
     music: ["Emmanuel Miranda"],
-    section: "design",
-    externalLink: "https://google.com",
+    section: "multimedia",
+    externalLink: "https://tutorahkids.tv/palabras-magicas",
     slides: [
       "/projects/magic-words/slide_1.jpg",
-      "/projects/magic-words/slide_2.jpg",
-      "/projects/magic-words/slide_4.jpg",
+      "/projects/magic-words/slide_2.jpg"
     ],
   };
 };
@@ -67,13 +68,20 @@ export const projectMillyCohenTales = (args?: { locale?: Locale }) => {
 
   return {
     ...info,
+    year: "2020",
+    client: "Tutorah Kids",
     videoUrl: "https://www.youtube.com/watch?v=fYH0K5Wk6vE",
-    illustration: [],
-    animation: [],
-    music: [],
+    illustration: ["Rafael Castillo"],
+    animation: ["Alejandro Olivares", "Gabriela Borraz"],
+    music: ["Rubén Garza"],
     section: "multimedia",
-    externalLink: "",
-    slides: [],
+    externalLink: "https://tutorahkids.tv/cuentos-de-milly",
+    slides: [      
+      "/projects/milly-cohen-tales/slide_1.jpg",
+      "/projects/milly-cohen-tales/slide_2.jpg",
+      "/projects/milly-cohen-tales/slide_3.jpg",
+      "/projects/milly-cohen-tales/slide_4.jpg"
+    ],
   };
 };
 
@@ -85,13 +93,19 @@ export const projectMidotPowerKids = (args?: { locale?: Locale }) => {
 
   return {
     ...info,
+    year: "2019",
+    client: "Tutorah Kids",
     videoUrl: "https://www.youtube.com/watch?v=-jO1qc-euZg",
-    illustration: [],
-    animation: [],
-    music: [],
+    illustration: ["Rafael Castillo"],
+    animation: ["Alejandro Olivares", "Gabriela Borraz"],
+    music: ["Emmanuel Miranda"],
     section: "multimedia",
-    externalLink: "",
-    slides: [],
+    externalLink: "https://tutorahkids.tv/midot-power-kids",
+    slides: [
+      "/projects/midot-power-kids/slide_1.jpg",
+      "/projects/midot-power-kids/slide_2.jpg",
+      "/projects/midot-power-kids/slide_3.jpg"
+    ],
   };
 };
 
@@ -103,13 +117,103 @@ export const projectTheViolaJourney = (args?: { locale?: Locale }) => {
 
   return {
     ...info,
-    videoUrl: "",
-    illustration: [],
-    animation: [],
-    music: [],
+    year: "2020",
+    client: "El Arte Ayuda",
+    videoUrl: "https://www.youtube.com/watch?v=TWjibLoA78U",
+    animation: ["Gabriela Borraz"],
+    music: ["Fermín Rodríguez"],
     section: "multimedia",
+    externalLink: "https://www.youtube.com/watch?v=gk_ZNO4zX7o",
+    slides: [
+      "/projects/the-viola-journey/slide_1.jpg",
+      "/projects/the-viola-journey/slide_2.jpg",
+      "/projects/the-viola-journey/slide_3.jpg"
+    ],
+  };
+};
+
+export const projectIntorahsante = (args?: { locale?: Locale }) => {
+  const locale = args?.locale;
+  const slug = "intorahsante";
+
+  const info = project({ slug, locale });
+
+  return {
+    ...info,
+    year: "2020",
+    client: "Tutorah Tv",
+    videoUrl: "https://www.youtube.com/watch?v=Edt_sDKYv6g",
+    animation: ["Alejandro Olivares", "Gabriela Borraz"],
+    music: ["Yosef Salinas"],
+    section: "multimedia",
+    externalLink: "https://youtu.be/Edt_sDKYv6g",
+    slides: [
+      "/projects/intorahsante/slide_1.jpg",
+      "/projects/intorahsante/slide_2.jpg",
+      "/projects/intorahsante/slide_3.jpg"
+    ],
+  };
+};
+
+export const projectNitzaStorytime = (args?: { locale?: Locale }) => {
+  const locale = args?.locale;
+  const slug = "nitza-storytime";
+
+  const info = project({ slug, locale });
+
+  return {
+    ...info,
+    year: "2020",
+    client: "Tutorah Kids",
+    videoUrl: "https://www.youtube.com/watch?v=0W9YGO_3FnQ",
+    illustration: ["Rafael Castillo"],
+    animation: ["Alejandro Olivares", "Gabriela Borraz"],
+    music: ["Alberto Camillin"],
+    section: "multimedia",
+    externalLink: "https://tutorahkids.tv/%C2%BFcuantos-cuentos-cuentas?",
+    slides: [
+      "/projects/nitza-storytime/slide_1.jpg",
+      "/projects/nitza-storytime/slide_2.jpg",
+      "/projects/nitza-storytime/slide_3.jpg"
+    ],
+  };
+};
+
+export const projectHelloAPI = (args?: { locale?: Locale }) => {
+  const locale = args?.locale;
+  const slug = "hello-api";
+
+  const info = project({ slug, locale });
+
+  return {
+    ...info,
+    year: "2023",
+    client: "Hello API",
+    section: "design",
     externalLink: "",
-    slides: [],
+    slides: [
+       "/projects/hello-api/slide_1.jpg",
+      "/projects/hello-api/slide_2.jpg"
+    ],
+  };
+};
+
+export const projectIxkik = (args?: { locale?: Locale }) => {
+  const locale = args?.locale;
+  const slug = "ixkik";
+
+  const info = project({ slug, locale });
+
+  return {
+    ...info,
+    year: "2017",
+    client: " Creative Lab",
+    section: "design",
+    externalLink: "",
+    slides: [
+      "/projects/ixkik/slide_1.jpg",
+      "/projects/ixkik/slide_2.jpg"
+    ],
   };
 };
 
@@ -118,4 +222,8 @@ export const projects: Projects = {
   "midot-power-kids": projectMidotPowerKids,
   "milly-cohen-tales": projectMillyCohenTales,
   "the-viola-journey": projectTheViolaJourney,
+  "intorahsante":projectIntorahsante,
+  "nitza-storytime": projectNitzaStorytime,
+  "hello-api": projectHelloAPI,
+  "ixkik": projectIxkik,
 };
