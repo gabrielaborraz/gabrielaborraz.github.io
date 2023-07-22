f1cec233cb2b4569bb9f034d1b07d93f11cdf0f1
Gabriela Borraz <139186642+gabrielaborraz@users.noreply.github.com>
2023-07-21 21:04:47 -0600

    Add Navigation Menu translations

--- src/utils/i18n/ui/navigation.ts
+++ src/utils/i18n/ui/navigation.ts
@@ -1,4 +1,14 @@
 export const navigation = {
-  es: {},
-  en: {},
+  es: {
+    "navigation.home":"Inicio",
+    "navigation.about":"Sobre mí",
+    "navigation.portfolio": "Portafolio",
+    "navigation.contact": "Contacto"
+  },
+  en: {
+    "navigation.home":"Home",
+    "navigation.about":"About",
+    "navigation.portfolio": "Portfolio",
+    "navigation.contact": "Contact"
+  },
 };
