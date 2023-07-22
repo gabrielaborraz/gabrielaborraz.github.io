7bcc617bf8c31c3ff7ddf942a28f7b9c3179e0eb
Gabriela Borraz <139186642+gabrielaborraz@users.noreply.github.com>
2023-07-21 20:29:08 -0600

    Add footer translations

--- src/utils/i18n/ui/footer.ts
+++ src/utils/i18n/ui/footer.ts
@@ -1,4 +1,34 @@
 export const footer = {
-  es: {},
-  en: {},
+  es: {
+    //MENU
+    "footer.menu": "Menú",
+    "footer.home":"Inicio",
+    "footer.about": "Sobre mí",
+    "footer.portfolio": "Portafolio",
+    //FOLLOW
+    "footer.follow": "Sígueme",
+    //ANSWERING
+    "footer.answer": "Te respondo",
+    "footer.contact": "Contacto",
+    //LANGUAGE
+    "footer.language": "Idioma",
+    //SUBSCRIBE
+    "footer.subscribe": "Suscríbete",
+  },
+  en: {
+    //MENU
+    "footer.menu": "Menu",
+    "footer.home":"Home",
+    "footer.about": "About",
+    "footer.portfolio": "Portfolio",
+    //FOLLOW
+    "footer.follow": "Follow me",
+    //ANSWERING
+    "footer.answer": "Answering",
+    "footer.contact": "Contact",
+    //LANGUAGE
+    "footer.language": "Language",
+    //SUBSCRIBE
+    "footer.subscribe": "Subscribe",
+  },
 };
