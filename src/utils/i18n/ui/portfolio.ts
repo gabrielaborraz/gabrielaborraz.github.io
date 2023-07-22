3d7cc2786052b3e6a05069f1dcee946483dcf717
Gabriela Borraz <139186642+gabrielaborraz@users.noreply.github.com>
2023-07-21 21:31:41 -0600

    Add breadcrumb and table-cell translations

--- src/utils/i18n/ui/portfolio.ts
+++ src/utils/i18n/ui/portfolio.ts
@@ -1,42 +1,60 @@
 export const portfolio = {
   es: {
-    "portfolio.index.title": "Portafolio de Proyectos",
-    "portfolio.index.description":
-      "Explora mi portafolio de proyectos y descubre cómo puedo ayudarte a producir una amplia variedad de videos y dar vida a tus ideas",
-    "portfolio.hero":
-      "La magia de los videos animados <span>comienza con una idea creativa</span>",
+    "portfolio.seo.title": "Portafolio de Proyectos",
+    "portfolio.seo.description": "Explora mi portafolio de proyectos y descubre cómo puedo ayudarte a producir una amplia variedad de videos y dar vida a tus ideas",
+    "portfolio.hero": "La magia de los videos animados <span>comienza con una idea creativa</span>",
     "portfolio.multimedia.title": "Explora",
     "portfolio.multimedia.subtitle": "Mi portafolio multimedia",
     "portfolio.design.title": "Descubre",
     "portfolio.design.subtitle": "Mi portafolio de diseño",
     /***********DESIGN***********/
-    "porfolio.design.index.title": "Portafolio de diseño",
-    "porfolio.design.index.description": "Explora mis proyectos de diseño",
+    "porfolio.design.seo.title": "Portafolio de diseño",
+    "porfolio.design.seo.description": "Explora mis proyectos de diseño",
     /***********MULTIMEDIA***********/
-    "portfolio.multimedia.index.title": "Portafolio multimedia",
-    "portfolio.multimedia.index.description":
-      "Descubre la magia de los videos animados",
-    /***********PROJECTS***********/
+    "portfolio.multimedia.seo.title": "Portafolio multimedia",
+    "portfolio.multimedia.seo.description": "Descubre la magia de los videos animados",
+    /***********BUTTONS AND BREADCRUMB***********/
     "portfolio.project.synopsis": "Sinopsis",
+    "portfolio.project.description": "Descripción",
+    "breadcrumb.home":"Inicio",
+    "breadcrumb.portfolio":"Portafolio",
+    "breadcrumb.multimedia":"Multimedia",
+    "breadcrumb.design":"Diseño",
+    "portfolio.external-btn" : "Ver →",
+    /***********CREDIT TABLE***********/
+    "portfolio.table-cell.year": "Año",
+    "portfolio.table-cell.client": "Cliente",
+    "portfolio.table-cell.illustration": "Ilustración",
+    "portfolio.table-cell.animation": "Animación",
+    "portfolio.table-cell.music": "Música",
   },
   en: {
-    "portfolio.index.title": "Project Portfolio ",
-    "portfolio.index.description":
-      "Explore my portfolio of projects and discover how I can help you to produce a wide variety of videos and bring your ideas to life",
-    "portfolio.hero":
-      "The magic of animated videos<span>starts with a creative idea</span>",
+    "portfolio.seo.title": "Project Portfolio ",
+    "portfolio.seo.description": "Explore my portfolio of projects and discover how I can help you to produce a wide variety of videos and bring your ideas to life",
+    "portfolio.hero": "The magic of animated videos<span>starts with a creative idea</span>",
     "portfolio.multimedia.title": "Explore",
     "portfolio.multimedia.subtitle": "My multimedia portfolio",
     "portfolio.design.title": "Discover",
     "portfolio.design.subtitle": "My design portfolio",
     /***********DESIGN***********/
-    "porfolio.design.index.title": "Design Portfolio",
-    "porfolio.design.index.description": "Explore my design projects",
+    "porfolio.design.seo.title": "Design Portfolio",
+    "porfolio.design.seo.description": "Explore my design projects",
     /***********MULTIMEDIA***********/
-    "portfolio.multimedia.index.title": "Multimedia Portfolio",
-    "portfolio.multimedia.index.description":
-      "Discover the magic of animated videos",
-    /***********PROJECTS***********/
+    "portfolio.multimedia.seo.title": "Multimedia Portfolio",
+    "portfolio.multimedia.seo.description": "Discover the magic of animated videos",
+    /***********BUTTONS AND BREADCRUMB***********/
     "portfolio.project.synopsis": "Synopsis",
+    "portfolio.project.description": "Description",
+    "breadcrumb.home":"Home",
+    "breadcrumb.portfolio":"Portfolio",
+    "breadcrumb.multimedia":"Multimedia",
+    "breadcrumb.design":"Design",
+    "portfolio.external-btn" : "Watch →",
+    /***********CREDIT TABLE***********/
+    "portfolio.table-cell.year": "Year",
+    "portfolio.table-cell.client": "Client",
+    "portfolio.table-cell.illustration": "Illustration",
+    "portfolio.table-cell.animation": "Animation",
+    "portfolio.table-cell.music": "Music",
   },
 };
